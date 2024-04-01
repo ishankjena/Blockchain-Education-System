c1a558273d990351795bd9c76d5dcb13d9865cb5
Ishank Jena <62696808+ishankjena@users.noreply.github.com>
2024-04-01 12:38:26 +0530

    Update CreatorRegistry.sol

--- CreatorRegistry.sol
+++ CreatorRegistry.sol
@@ -1,1 +1,23 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+contract CreatorRegistry {
+    struct Creator {
+        string name;
+        uint256 verifiedIdentity;
+        string academicHistory;
+    }
+
+    mapping(address => Creator) public creators;
+
+    event CreatorRegistered(address indexed creatorAddress, string name);
+
+    function registerCreator(string memory _name, uint256 _verifiedIdentity, string memory _academicHistory) public {
+        require(creators[msg.sender].verifiedIdentity == 0, "Creator already registered");
+
+        creators[msg.sender] = Creator(_name, _verifiedIdentity, _academicHistory);
+        emit CreatorRegistered(msg.sender, _name);
+    }
+}
 
