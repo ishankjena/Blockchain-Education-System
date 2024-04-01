d8e8775b036d04a2f8f46b5518a898ad2ca9f614
Ishank Jena <62696808+ishankjena@users.noreply.github.com>
2024-04-01 12:50:34 +0530

    Update Grade.sol

--- contracts/Grade.sol
+++ contracts/Grade.sol
@@ -1,1 +1,29 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import "./StudentRegistry.sol";
+
+contract Grader {
+    struct Grade {
+        address student;
+        uint256 grade;
+    }
+
+    mapping(address => Grade) public grades;
+
+    StudentRegistry public studentRegistry;
+
+    event GradeAssigned(address indexed student, uint256 grade);
+
+    constructor(address _studentRegistry) {
+        studentRegistry = StudentRegistry(_studentRegistry);
+    }
+
+    function assignGrade(address _student, uint256 _grade) public {
+        require(studentRegistry.isStudentRegistered(_student), "Student not registered");
+        grades[_student] = Grade(_student, _grade);
+        emit GradeAssigned(_student, _grade);
+    }
+}
 
